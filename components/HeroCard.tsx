a972d5adbc80b396a6409f126372b23979958a4c
Godspower Similoluwa <gpsimi01@gmail.com>
2024-08-16 17:11:04 +0100

    Update HeroCard.tsx

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import { Button } from './ui/button'
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const HeroCard = () => {
-    return (
-        <section className='max-md:pb-[500px] pb-[100px]'>
-            <div className="container mx-auto max-w-[1200px] px-4 py-16">
-                <div className="absolute -mt-[180px] left-0 right-0 container max-w-[1280px] mx-auto py-10">
-                    <div className="grid grid-cols-1 md:grid-cols-3">
-                        <div className="bg-teal-500 py-8 px-6">
-                            <h3 className="text-[20px] md:text-[25px] font-bold font-serif text-white">
-                                Education
-                            </h3>
-                            <p className="text-white mt-2">
-                                Below Poverty Line is a benchmark used by the government of India
-                                to indicate economic disadvantage.
-                            </p>
-                            <Button className="bg-white text-teal-500 mt-4">Discover</Button>
-                        </div>
-                        <div className="bg-gray-800 py-8 px-6">
-                            <h3 className="text-[20px] md:text-[25px] font-bold font-serif text-white">
-                                Become a Volunteer
-                            </h3>
-                            <p className="text-white mt-2">
-                                Below Poverty Line is a benchmark used by the government.
-                            </p>
-                            <Button className="bg-white text-gray-800 mt-4">Join Organization</Button>
-                        </div>
-                        <div className="bg-red-500 py-8 px-6">
-                            <h3 className="text-[20px] md:text-[25px] font-bold font-serif text-white">
-                                <span className="block">Join</span> Organization
-                            </h3>
-                            <p className="text-white mt-2">
-                                Below Poverty Line is a benchmark used by the government.
-                            </p>
-                            <Button className="bg-white text-red-500 mt-4">Join Organization</Button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="max-md:pb-[500px] pb-[100px]">
+      <div className="container mx-auto max-w-[1200px] px-4 py-16">
+        <div className="absolute -mt-[180px] left-0 right-0 container max-w-[1280px] mx-auto py-10">
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            
+            <div className="bg-green-1 flex-start">
+              <h3 className="herocardHeader text-dark-1 max-w-[250px]">
+                Education
+              </h3>
+              <p className="text-dark-1 herocardText">
+                Below Poverty Line is enchmark used by the government of India
+                economic disadvantage.
+              </p>
+              <Button className="bg-white text-dark-1  mt-4 hero-button">
+                Discover
+              </Button>
             </div>
-        </section>
-    )
-}
 
-export default HeroCard
+            <div className="bg-dark-1 flex-start">
+              <h3 className="herocardHeader text-white">
+                Become <br /> a Volunteer
+              </h3>
+              <p className="text-white herocardText">
+                Below Poverty Line is enchmark used by the government
+              </p>
+              <Link className="text-white herocardText uppercase underline underline-offset-2 hover:no-underline" href="/our-causes">Join Organization</Link>
+            </div>
+            
+            <div className="bg-red-500 flex-start">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroCard;
