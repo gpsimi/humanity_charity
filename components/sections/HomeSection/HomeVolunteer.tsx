7cc29006ca957849687266b1f8a57ab75ec74aed
Godspower Similoluwa <gpsimi01@gmail.com>
2024-08-20 14:26:21 +0100

    Update HomeVolunteer.tsx

--- components/sections/HomeSection/HomeVolunteer.tsx
+++ components/sections/HomeSection/HomeVolunteer.tsx
@@ -4,13 +4,13 @@ const HomeVolunteer = () => {
   return (
     <section className='md:py-24 py-12 bg-homeVolunteers relative bg-cover bg-center bg-no-repeat w-full h-[400px]'>
       <div className='absolute w-full top-0 left-0 glassmorphism2 h-[400px]'/>  
-        <div className='container mx-auto max-w-[1280px]'>
+        <div className='relative container mx-auto max-w-[1280px] z-50'>
           <div className='flex flex-col md:flex-row gap-8'>
             <div className='w-full md:w-1/2'>
-
+              <p className='text-white'>Left</p>
             </div>
             <div className='w-full md:w-1/2'>
-              
+              <p className='text-white'>Right</p>
             </div>
           </div>
         </div>
