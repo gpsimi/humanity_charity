22b134e844f19869e03c11b98ceca164c665d8fe
Godspower Similoluwa <gpsimi01@gmail.com>
2024-08-22 16:50:14 +0100

    Update HomeTestimonialsCard.tsx

--- components/HomeTestimonialsCard.tsx
+++ components/HomeTestimonialsCard.tsx
@@ -4,9 +4,9 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import React, { useRef } from "react";
 import Slider from "react-slick";
 import { Button } from "./ui/button";
-import { homeCauses } from "@/constants/page";
 import Image from "next/image";
 import Link from "next/link";
+import { homeTestimonial } from "@/constants/page";
 
 
 const HomeTestimonialsCard = () => {
@@ -29,7 +29,7 @@ const HomeTestimonialsCard = () => {
             {
                 breakpoint: 10000,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: true,
                 },
@@ -61,50 +61,30 @@ const HomeTestimonialsCard = () => {
                     ref={sliderRef}
                     {...settings}
                 >
-
-                    {homeCauses.map((donation) => (
-                        <div key={donation.id} className="">
-                            <div className="relative">
+                    {homeTestimonial.map((item) =>(
+                        <div key={item.id} className="relative flex space-x-10">
+                            <div className="space-x-9">
+                            <div className="bg-gray-2 flex p-5">
                                 <Image
-                                    src={donation.image}
-                                    alt={donation.title}
-                                    width={400}
-                                    height={50}
-                                    className="object-cover md:w-[400px] w-[350px]"
+                                    src={item.image}
+                                    alt={item.name}
+                                    width={150}
+                                    height={60}
+                                    className="h-full"
                                 />
-                                <div className="absolute top-5 left-4 bg-white font-bold font-serif px-2 py-1 text-orange-1">
-                                    {donation.percentage}
-                                </div>
+                                h
                             </div>
-                            <div className="p-4 md:p-6 bg-white md:w-[400px] w-[350px]">
-                                <h3 className="h3">
-                                    {donation.title}
-                                </h3>
-                                <div className="relative pt-1">
-                                    <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-4">
-                                        <div
-                                            style={{ width: donation.percentage }}
-                                            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-orange-1"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="flex justify-between text-gray-7000 mb-4">
-                                    <span className='spanDark'>Raised - {donation.raised}</span>
-                                    <span className='spanOrange'>Goal - {donation.goal}</span>
-                                </div>
-
-                                <Button className="button-dark max-md:mb-3">
-                                    <Link href={`/causes/${donation.id}`}>Donate</Link>
-                                </Button>
                             </div>
                         </div>
                     ))}
+                    
 
                 </Slider>
-                <div className="mt-6 flex gap-12 justify-center">
+                <div className=" mt-6 md:mt-12 flex gap-8 md:gap-32 justify-center">
                     <Button variant="outline" className="bg-white rounded-full" size="icon" onClick={previous}>
                         <ChevronLeft className="h-5 w-8 text-dark-1" />
                     </Button>
+                    <hr className="border-black mb-5" />
                     <Button variant="outline" className="bg-white rounded-full" size="icon" onClick={next}>
                         <ChevronRight className="h-5 w-8 text-dark-1" />
                     </Button>
