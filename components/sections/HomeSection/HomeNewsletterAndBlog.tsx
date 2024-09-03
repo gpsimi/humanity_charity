5261a53f0c722f58758f42d75ff036ff42e8ede5
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-03 15:58:22 +0100

    Update

--- components/sections/HomeSection/HomeNewsletterAndBlog.tsx
+++ components/sections/HomeSection/HomeNewsletterAndBlog.tsx
@@ -38,8 +38,9 @@ const HomeNewsletterAndBlog = () => {
             </div>
 
             {/* Recent Blog Section */}
-            <div className="flex flex-col justify-center  bg-green-1 p-6 md:p-16 w-full md:w-1/2 text-white">
-                <div className=' max-w-[600px]'>
+            <div className="relative flex flex-col justify-center bg-homeBlog p-6 md:p-16 w-full md:w-1/2 text-white">
+                <div className="absolute inset-0 bg-green-1 opacity-90" />
+                <div className=' max-w-[600px] z-30'>
                     <h5 className="bodySubtext-dark">
                         Recent Blog
                     </h5>
