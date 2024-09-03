ffaea6c3067d8a31acf76568aa9515a7baa934fe
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-03 15:26:20 +0100

    Update

--- components/sections/HomeSection/HomeNewsletterAndBlog.tsx
+++ components/sections/HomeSection/HomeNewsletterAndBlog.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const HomeNewsletterAndBlog = () => {
     return (
-        <div className="flex flex-col md:flex-row w-full">
+        <div className="flex flex-col md:flex-row w-full mx-auto">
             {/* Subscribe Newsletter Section */}
-            
+
             <div className="flex flex-col items-start justify-center bg-orange-1 p-12 w-full md:w-1/2 text-white">
                 <div className='container max-w-[600px]'>
                     <h5 className="bodySubtext-white">
@@ -19,7 +19,7 @@ const HomeNewsletterAndBlog = () => {
                         Sign up for our monthly newsletter to
                         get the latest news, volunteer opportunities,
                     </p>
-                    <div  className="">
+                    <div className="">
 
                         <div className='flex space-x-3'>
                             <input
@@ -38,8 +38,8 @@ const HomeNewsletterAndBlog = () => {
             </div>
 
             {/* Recent Blog Section */}
-            <div className="flex flex-col justify-center bg-homeFooter w-full md:w-1/2 text-white">
-                <div className='container max-w-[600px]'>
+            <div className="flex flex-col justify-center  bg-green-1 p-16 w-full md:w-1/2 text-white">
+                <div className=' max-w-[600px]'>
                     <h5 className="bodySubtext-dark">
                         Recent Blog
                     </h5>
