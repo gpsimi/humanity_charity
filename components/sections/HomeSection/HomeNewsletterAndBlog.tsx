3688eb5ec84d367bc0dc264ec69fa9da90472f41
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-03 15:06:22 +0100

    Update

--- components/sections/HomeSection/HomeNewsletterAndBlog.tsx
+++ components/sections/HomeSection/HomeNewsletterAndBlog.tsx
@@ -6,6 +6,7 @@ const HomeNewsletterAndBlog = () => {
     return (
         <div className="flex flex-col md:flex-row w-full">
             {/* Subscribe Newsletter Section */}
+            
             <div className="flex flex-col items-start justify-center bg-orange-1 p-12 w-full md:w-1/2 text-white">
                 <div className='container max-w-[600px]'>
                     <h5 className="bodySubtext-white">
@@ -18,15 +19,29 @@ const HomeNewsletterAndBlog = () => {
                         Sign up for our monthly newsletter to
                         get the latest news, volunteer opportunities,
                     </p>
+                    <div  className="">
+
+                        <div className='flex space-x-3'>
+                            <input
+                                // {...register('email')}
+                                placeholder="Enter Your Email Address"
+                                className="inputStyle-1 rounded-full"
+                            />
+                            {/* {errors.email && <span className="inputSpan">{errors.email.message}</span>} */}
+                            <Button type="submit" className="mt-4 button-dark ">
+                                Send
+                            </Button>
+                        </div>
+                    </div>
                 </div>
 
             </div>
 
             {/* Recent Blog Section */}
-            <div className="flex flex-col items-start justify-center bg-green-1 p-12 w-full md:w-1/2 text-white">
+            <div className="flex flex-col justify-center bg-green-1 w-full md:w-1/2 text-white">
                 <div className='container max-w-[600px]'>
                     <h5 className="bodySubtext-dark">
-                       Recent Blog
+                        Recent Blog
                     </h5>
                     <h3 className="bodyText-dark max-w-[500px]">
                         Take Look At Recent Blog Posts
