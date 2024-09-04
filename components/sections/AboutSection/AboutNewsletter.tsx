6e294dc1998dc9949714d137e561c0206728a895
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-04 22:14:16 +0100

    Create AboutNewsletter.tsx

---
+++ components/sections/AboutSection/AboutNewsletter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutNewsletter = () => {
+  return (
+    <section className='py-12 md:py-24'>
+        <div className="container mx-auto max-w-[1280px]">
+            AboutNewsletter
+        </div>
+    </section>
+  )
+}
+
+export default AboutNewsletter
