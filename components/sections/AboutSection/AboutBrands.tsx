4df1630730be7c65a87b74d1f9cf50a4f72c8996
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-04 18:32:48 +0100

    Update AboutBrands.tsx

--- components/sections/AboutSection/AboutBrands.tsx
+++ components/sections/AboutSection/AboutBrands.tsx
@@ -2,14 +2,15 @@ import Brands from '@/components/Brands'
 import React from 'react'
 
 const AboutBrands = () => {
-  return (
-    <section className='py-12 md:py-24 '>
-        <div className="container mx-auto max-w-[1280px] max-md:px-10">
-            <Brands />
-        </div>
-        
-    </section>
-  )
+    return (
+        <section className='py-8 md:py-16 '>
+            <div className="container mx-auto max-w-[1280px] max-md:px-10">
+                <div className="flex-1 h-[2px] bg-border-1/50 mb-8" />
+                <Brands />
+                <div className="flex-1 h-[2px] bg-border-1/50 mt-6" />
+            </div>
+        </section>
+    )
 }
 
 export default AboutBrands
