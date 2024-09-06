c802d559bf95437cd8372e21fc66752f4d137ae1
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-06 10:37:25 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-
-import HomeAbout from "@/components/sections/HomeSection/HomeAbout";
-import HomeCauses from "@/components/sections/HomeSection/HomeCauses";
-import HomeHero from "@/components/sections/HomeSection/HomeHero";
-import HomeProjects from "@/components/sections/HomeSection/HomeProjects";
-import HomeTestimonials from "@/components/sections/HomeSection/HomeTestimonials";
-import HomeVolunteer from "@/components/sections/HomeSection/HomeVolunteer";
-import HomeWhatWeDo from "@/components/sections/HomeSection/HomeWhatWeDo";
-import HomeSponsors from "@/components/sections/HomeSection/HomeSponsors";
-import HomeTeam from "@/components/sections/HomeSection/HomeTeam";
-import HomeEvents from "@/components/sections/HomeSection/HomeEvents";
-import HomeNewsletterAndBlog from "@/components/sections/HomeSection/HomeNewsletterAndBlog";
-
+import {
+  HomeHero,
+  HomeAbout,
+  HomeCauses,
+  HomeVolunteer,
+  HomeWhatWeDo,
+  HomeProjects,
+  HomeTestimonials,
+  HomeSponsors,
+  HomeTeam,
+  HomeEvents,
+  HomeNewsletterAndBlog
+} from "@/components/sections/HomeSection/index"
 
 
            
