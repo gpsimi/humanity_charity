700db4fc5dde68af557ae724d14abb4b9fcd8750
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-07 21:01:39 +0100

    Update HomeVolunteer.tsx

--- components/sections/HomeSection/HomeVolunteer.tsx
+++ components/sections/HomeSection/HomeVolunteer.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const HomeVolunteer = () => {
   return (
     <section className='flex items-center md:py-24 py-12 bg-homeVolunteers relative bg-cover bg-center bg-no-repeat w-full h-full'>
-      <div className='absolute top-0 left-0 glassmorphism2 w-full h-full' />
+      <div className='absolute inset-0 glassmorphism2 w-full' />
       <div className='container mx-auto max-w-[1280px] z-30'>
         <div className='flex flex-col md:flex-row gap-8 items-center justify-end'>
 
