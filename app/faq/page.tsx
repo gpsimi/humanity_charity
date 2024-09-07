bac6e8c9794ce8bd526e2eeaa3d841158084d4e2
Godspower Similoluwa <gpsimi01@gmail.com>
2024-09-07 21:37:16 +0100

    Update page.tsx

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,9 +1,11 @@
+import { FaqGroup, FaqHero } from '@/components/sections/FaqSection'
 import React from 'react'
 
 const Faq = () => {
   return (
     <section className="flex size-full flex-col">
-    
+    <FaqHero />
+    <FaqGroup />
   </section>
   )
 }
