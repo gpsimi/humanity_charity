1eeb664bc2949d2e411abf53987743266324807f
Godspower <gpsimi01@gmail.com>
2024-11-09 11:58:36 +0100

    Update BlogHero.tsx

--- components/sections/BlogSection/BlogHero.tsx
+++ components/sections/BlogSection/BlogHero.tsx
@@ -8,10 +8,9 @@ const BlogHero = () => {
     return (
         <section className="flex h-screen md:h-[60vh] w-full bg-blog bg-cover bg-center relative">
             <div className="container mx-auto md:max-w-[1280px] flex flex-col h-full justify-center items-center lg:items-end z-30">
-                <div className="bg-white p-12 flex flex-col space-y-5 max-w-lg  item-start">
+                <div className="bg-white p-12 flex flex-col space-y-3 max-w-lg item-start">
                     <div className='flex items-start justify-start'>
-
-                        <h4 className="uppercase">
+                        <h4 className="bodyParagraph-dark uppercase">
                             <span className="">
                                 March 16, 2022
                             </span>
@@ -22,7 +21,7 @@ const BlogHero = () => {
                         </h4>
 
                     </div>
-                    <h2 className='bodySubtext-dark'>Why Giving Is Impportant | Five Reaons to Donate</h2>
+                    <h2 className='bodyText-dark-3'>Why Giving Is Impportant | Five Reaons to Donate</h2>
                     <p className='bodyParagraph-dark'>
                         There are many variations of passages of Lorem
                         Ipsum available, but the majority have suffered
