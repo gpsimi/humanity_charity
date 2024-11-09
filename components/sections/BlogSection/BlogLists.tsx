bab99ba8219ebcbe73f08d564fa7cf914341410d
Godspower <gpsimi01@gmail.com>
2024-11-09 12:04:09 +0100

    Update BlogLists.tsx

--- components/sections/BlogSection/BlogLists.tsx
+++ components/sections/BlogSection/BlogLists.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const BlogLists = () => {
   return (
-    <div>
-      Blog List
-    </div>
+    <section className='py-12 lg:py-24'>
+      <div className="container mx-auto flex flex-col justify-center items-center space-y-6 ">
+        <div>
+          BlogList
+        </div>
+        <div>
+          BlogList
+        </div>
+        <div>
+          BlogList
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
