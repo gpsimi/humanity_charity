15cf66845b94f9f1ddb86a42cbd777f366ba1bfa
Godspower <gpsimi01@gmail.com>
2024-11-09 19:54:38 +0100

    Update BlogHero.tsx

--- components/sections/BlogSection/BlogHero.tsx
+++ components/sections/BlogSection/BlogHero.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-import { HomeHero } from "@/constants/page";
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+import React from 'react';
+import { Button } from '@/components/ui/button';
 
 
 const BlogHero = () => {
